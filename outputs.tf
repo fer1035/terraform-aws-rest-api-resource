4b03f44b6ea6e09cf0bf24219c95b42b05e3c674
Ahmad Ferdaus Abd Razak <ahmad.ferdaus.abd.razak@ni.com>
2022-04-07 11:51:16 +0800

    Initial load for module files.

---
+++ outputs.tf
@@ -0,0 +1,4 @@
+output "resource_id" {
+  value       = aws_api_gateway_resource.resource.id
+  description = "REST API resource ID."
+}
---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "path_part" {
+  type        = string
+  description = "The path part for the endpoint."
+}
+
+variable "parent_id" {
+  type        = string
+  description = "The parent resource ID of the REST API in API Gateway."
+}
+
+variable "api_id" {
+  type        = string
+  description = "The ID of the REST API in API Gateway."
+}
